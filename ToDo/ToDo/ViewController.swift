3d6adbf2123850fe6fe04f2d760cddc5b2b03cfe
Lauren Small <lauren.small@detroitlabs.com>
2019-01-16 10:39:54 -0500

    add README and empty project

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Lauren Small on 1/16/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
