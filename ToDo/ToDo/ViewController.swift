e7bb5f630419ba1b795264a1126d901f0ec0374b
Lauren Small <lauren.small@detroitlabs.com>
2019-01-16 16:22:55 -0500

    add UITableViewDataSource methods

--- ToDo/ToDo/ViewController.swift
+++ ToDo/ToDo/ViewController.swift
@@ -26,3 +26,25 @@ class ViewController: UIViewController {
     
 }
 
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                              for: indexPath)
+            cell.textLabel?.text = toDoList[indexPath.row]
+            return cell
+    }
+}
+
+
